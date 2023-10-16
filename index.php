8068b3518c329bbd7f3ca790a0806c58dd93fc38
Thuy Tien Luong <0203@htl.rennweg.at>
2023-10-16 09:34:52 +0200

    andere vids

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ $ytv3 = new YouTubeVideo("Nutrition for a Healthy Life", "https://www.youtube.co
 $ytv4 = new YouTubeVideo("Nutrition: Importance of the Five Food Groups", "https://www.youtube.com/embed/47k0VUE6aZU?si=Gaf_JbHIwQY1zRVJ");
 $ytv5 = new YouTubeVideo("GCSE Biology -What are Nutrients?", "https://www.youtube.com/embed/dN7c2JniR8U");
 
-$vv1 = new VimeoVideo("Infographics Nutrition", "https://player.vimeo.com/video/64981602");
-$vv2 = new VimeoVideo("Food-Radames", "https://player.vimeo.com/video/27138679");
+$vv1 = new VimeoVideo("Healthy Food, Healthy Digital Marketing", "https://player.vimeo.com/video/584555961");
+$vv2 = new VimeoVideo("Bringing Healthy Food to Food Deserts", "https://player.vimeo.com/video/33784977");
 $vv3 = new VimeoVideo("Healthy Food Transisition", "https://player.vimeo.com/video/79898890");
 $vv4 = new VimeoVideo("Healthy Food, Healthy Body, Healthy Mind", "https://player.vimeo.com/video/8063633");
-$vv5 = new VimeoVideo("The Healthy Eating Song", "https://player.vimeo.com/video/466933445");
+$vv5 = new VimeoVideo("Healthy Food Healthy Planet - Retail", "https://player.vimeo.com/video/862921737");
 
 $header = <<<HEADER
 <!DOCTYPE html>
